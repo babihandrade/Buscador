93d31b8bb58521b553c4a31b0efcb509a5b96143
31687059 <31687059@089915-P33S11.macklabs.br>
2017-11-01 17:52:21 -0200

    Criação do Projeto Buscador

--- src/buscador/model/Loja.java
+++ src/buscador/model/Loja.java
@@ -5,6 +5,7 @@
  */
 package buscador.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,5 +15,61 @@ import java.util.List;
 public class Loja {
     private String nome;
     private String link;
-    private static int qtdLojas;
-    private List<Produto> produtos;
+    private static int qtdLojas = 0;
+    private List<Produto> produtos = new ArrayList();
+
+    public Loja(String nome, String link) {
+        this.nome = nome;
+        this.link = link;
+        qtdLojas++ ;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public static int getQtdLojas() {
+        return qtdLojas;
+    }
+
+    public static void setQtdLojas(int qtdLojas) {
+        Loja.qtdLojas = qtdLojas;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+    
+    
+    public void adicionaProdutos(Produto produto){
+    
+        
+    }
+    
+    public void alteraNome(String nome){
+    
+    }
+    
+    public void alteraLink(String link){
+    
+    
+    }
+
+}
+
